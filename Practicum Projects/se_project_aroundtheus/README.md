4c977237ea60799cb0a7e1adf240cde96baab953
PracticumStudent <PLouis53@gmail.com>
2023-01-20 17:21:44 -0500

    1/20/23 Updates PT.1

---
+++ Practicum Projects/se_project_aroundtheus/README.md
@@ -0,0 +1,36 @@
+# Project 3: Around The U.S.
+
+By Phillippe Louis
+
+Overview
+
+- Intro
+- Figma
+- Images
+-GitHub
+
+Project Description and Tech Functionality
+
+This project is a website containing a profile for Jacques Cousteau and different cards containing names and images off different areas in the U.S. This project shows how to create an HTML file and CSS files to create a website that works at resolutions as high at 1280px (desktop) and as small as 320px (mobile). The site includes a header, main content, and footer. All of the buttons on the site have interactive hovers. The site displays different cards using a grid layout
+
+This project is made so all the elements are displayed correctly on popular screen sizes. We recommend investing more time in completing this project, since it's more difficult than previous ones.
+
+Technologies Used
+
+HTML
+CSS
+Figma
+GitHub
+
+**Figma**
+
+- [Link to the project on Figma](https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)
+
+Project Screenshot:
+
+![Screenshot 2023-01-16 at 9 46 11 PM](https://user-images.githubusercontent.com/117612222/212801482-49e6f755-cb12-420a-9a3f-b95ed521b2af.png)
+
+
+
+GitHub Pages
+https://plouis53.github.io/se_project_aroundtheus/
---
+++ Practicum Projects/se_project_aroundtheus/scripts/index.js
@@ -0,0 +1,92 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+/* Elements*/
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditclose = document.querySelector("#profile-edit-close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListElement = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+/* Functions*/
+
+function closePopop() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = cardData.name;
+  cardTitleElement.textContent = cardData.name;
+  return cardElement;
+}
+
+/* Event Handler */
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopop();
+}
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
+});
+profileEditclose.addEventListener("click", () => {
+  closePopUp();
+});
+
+/* Event Listener */
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileEditClose.addEventListener("click", closePopop);
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListElement.append(cardElement);
+});
