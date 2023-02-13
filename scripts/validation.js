49bb7d2980baf673b5aabc04ef6cab0054766e17
PracticumStudent <PLouis53@gmail.com>
2023-02-13 17:04:30 -0500

    Update HTML & VALIDATION PT.4

--- scripts/validation.js
+++ scripts/validation.js
@@ -16,33 +16,37 @@ function enableValidation(options) {
 
 /*2*/
 function setEventListeners(formElements, options) {
-    const { inputSelector } = options;
-    const { submitButtonSelector } = options;
-    const inputList = [...formElements.querySelectorAll(inputSelector)];
-    const submitButton = formElements.querySelector(submitButtonSelector);
-  
-    formElements.addEventListener("reset", () => {
-      setTimeout(() => {
-        toggleButtonState(inputList, submitButton, options);
-      });
-    });
-  
-    inputList.forEach((inputElements) => {
-      inputElements.addEventListener("input", (e) => {
-        checkInputValidity(formElements, inputElements, options);
-        toggleButtonState( inputList, submitButton, options);
-      });
-    });
-  }
-  /*3*/
-  function checkInputValidity(formElements, inputElements, options) {
-    if (!inputElements.validity.valid) {
-      return showInputError(formElements, inputElements, options);
-    }
-    hideInputError(formElements, inputElements, options);
-  }
+  const { inputSelector } = options;
+  const { submitButtonSelector } = options;
+  const inputList = [...formElements.querySelectorAll(inputSelector)];
+  const submitButton = formElements.querySelector(submitButtonSelector);
 
-function showInputError(formElements, inputElements, options {inputErrorClass, errorClass}) {
+  formElements.addEventListener("reset", () => {
+    setTimeout(() => {
+      toggleButtonState(inputList, submitButton, options);
+    });
+  });
+
+  inputList.forEach((inputElements) => {
+    inputElements.addEventListener("input", (e) => {
+      checkInputValidity(formElements, inputElements, options);
+      toggleButtonState(inputList, submitButton, options);
+    });
+  });
+}
+/*3*/
+function checkInputValidity(formElements, inputElements, options) {
+  if (!inputElements.validity.valid) {
+    return showInputError(formElements, inputElements, options);
+  }
+  hideInputError(formElements, inputElements, options);
+}
+
+function showInputError(
+  formElements,
+  inputElements,
+  { inputErrorClass, errorClass }
+) {
   const errorMessageElements = formElements.querySelector(
     `#${inputElements.id}-error`
   );
@@ -60,24 +64,25 @@ function hideInputError(
     `#${inputElements.id}-error`
   );
   inputElements.classList.remove(inputErrorClass);
+  console.log(`#${inputElements.id}-error`);
   errorMessageElements.textContent = " ";
   errorMessageElements.classList.remove(errorClass);
 }
-function toggleButtonState( inputList, submitButton{inactiveButtonClass}){
-    let foundInvalid= false;
+function toggleButtonState(inputList, submitButton, { inactiveButtonClass }) {
+  let foundInvalid = false;
 
-    inputList.forEach((inputElements) =>{
-        if (!inputElements.validity.valid){
-            foundInvalid=true;
-        }
-    });
-
-    if(foundInvalid){
-        submitButton.classList.add(inactiveButtonClass); 
-        return( submitButton.disabled=true);
+  inputList.forEach((inputElements) => {
+    if (!inputElements.validity.valid) {
+      foundInvalid = true;
     }
-    submitButton.classList.remove(inactiveButtonClass);
-    submitButton.disabled=false;
+  });
+
+  if (foundInvalid) {
+    submitButton.classList.add(inactiveButtonClass);
+    return (submitButton.disabled = true);
+  }
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
 }
 
 /* ---------------------------- Validation Object --------------------------- */
@@ -85,8 +90,8 @@ const options = {
   formSelector: ".modal__form",
   inputSelector: ".modal__form-input",
   submitButtonSelector: ".modal__form-button",
-  inactiveButtonClass: ".modal__button_disabled",
-  inputErrorClass: ".modal__input_type_error",
+  inactiveButtonClass: ".modal__form-button_inactive",
+  inputErrorClass: ".modal__form-input_error",
   errorClass: ".modal__error_visible",
 };
 
