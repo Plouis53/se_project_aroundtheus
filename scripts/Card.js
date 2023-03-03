d249febec2dc41113963f899bbd57156ca9da3c0
PracticumStudent <PLouis53@gmail.com>
2023-03-02 19:22:21 -0500

    Add class in Card.js PT.1

---
+++ scripts/Card.js
@@ -0,0 +1,8 @@
+export default class Card {
+  constructor({ name, link }) {
+    this.name = name;
+    console.log(this);
+  }
+}
+
+testMethod (){}
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import Card from "./Card.js";
+
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -24,6 +26,13 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const cardData = {
+  name: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+
+const card = new Card(cardData);
 
 /* Elements*/
 //---Sprint 4 -->
