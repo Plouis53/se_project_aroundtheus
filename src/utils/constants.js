e1c30527c1075dd02884f9962fdc1dc820560532
PracticumStudent <PLouis53@gmail.com>
2023-03-16 20:49:56 -0400

    Update Index.css & constants.js PT.3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,12 @@
-const initialCards = [
+export const validationSettings = {
+  inputSelector: ".modal__form-input",
+  submitButtonSelector: ".modal__form-button",
+  inactiveButtonClass: "modal__form-button_inactive",
+  inputErrorClass: "modal__form-input_error",
+  errorClass: "modal__error_visible",
+};
+
+export const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
@@ -25,12 +33,23 @@ const initialCards = [
   },
 ];
 
-const validationSettings = {
-  inputSelector: ".modal__form-input",
-  submitButtonSelector: ".modal__form-button",
-  inactiveButtonClass: "modal__form-button_inactive",
-  inputErrorClass: "modal__form-input_error",
-  errorClass: "modal__error_visible",
-};
+export const profileEditModal = document.querySelector("#profile-edit-modal");
+export const addNewCardModal = document.querySelector("#card-add-modal");
+export const profileEditForm = profileEditModal.querySelector(".modal__form");
+export const cardAddForm = addNewCardModal.querySelector(
+  "#modal-add-card-form"
+);
 
-export { initialCards, validationSettings };
+export const profileEditButton = document.querySelector("#profile-edit-button");
+export const addNewCardButton = document.querySelector("#profile-add-button");
+export const profileTitle = document.querySelector(".profile__title");
+export const profileDescription = document.querySelector(
+  ".profile__description"
+);
+export const profileTitleInput = document.querySelector("#profile-title-input");
+export const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+export const cardTitleInput = document.querySelector("#modal-card-title-input");
+export const cardImageInput = document.querySelector("#modal-card-image-input");
+export const cardListElement = document.querySelector(".cards__list");
