c5bfa1a6b8e8438be066e78125fc827e9c4cdde1
PracticumStudent <PLouis53@gmail.com>
2023-03-25 13:57:03 -0400

    Revise src/components/UserInfo.js

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,10 +1,4 @@
 class UserInfo {
-  static getUserInfo() {
-    throw new Error("Method not implemented.");
-  }
-  static setUserInfo(arg0) {
-    throw new Error("Method not implemented.");
-  }
   constructor({ nameSelector, descriptionSelector }) {
     this._nameElement = document.querySelector(nameSelector);
     this._descriptionElement = document.querySelector(descriptionSelector);
@@ -17,9 +11,9 @@ class UserInfo {
     };
   }
 
-  setUserInfo({ name, description }) {
+  setUserInfo({ name, job }) {
     this._nameElement.textContent = name;
-    this._descriptionElement.textContent = description;
+    this._descriptionElement.textContent = job;
   }
 }
 
