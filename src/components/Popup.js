22e2139066f3f04b7a724f496596dcc517a118d8
PracticumStudent <PLouis53@gmail.com>
2023-03-25 13:34:36 -0400

    revise src/components/Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,46 +1,38 @@
 class Popup {
   constructor({ popupSelector }) {
-    this._popup = document.querySelector(popupSelector);
-
-    this._CloseButton = this._popup.querySelector("modal__close");
-    this.open = this.open.bind(this);
-    this.close = this.close.bind(this);
-    this._handleEscClose = this._handleEscClose.bind(this);
-    this._handleOverlay = this._handleOverlay.bind(this);
+    this._popupElement = document.querySelector(popupSelector);
   }
 
   open() {
-    this._popup.classList.add("modal_opened");
-    this.setEventListeners();
+    this._popupElement.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscapeClose);
+    document.addEventListener("mousedown", this._handleOverlay);
   }
 
   close() {
-    this._popup.classList.remove("modal_opened");
-    this.removeEventListeners();
+    this._popupElement.classList.remove("modal_opened");
+    document.removeEventListener("keydown", this._handleEscapeClose);
+    document.removeEventListener("mousedown", this._handleOverlay);
   }
 
-  _handleEscClose(e) {
+  _handleEscapeClose = (e) => {
     if (e.key === "Escape") {
       this.close();
     }
-  }
+  };
 
-  _handleOverlay(e) {
+  _handleOverlay = (e) => {
     if (e.target.classList.contains("modal_opened")) {
       this.close();
     }
-  }
+  };
 
   setEventListeners() {
-    document.addEventListener("keydown", this._handleEscClose);
-    this._popup.addEventListener("mousedown", this._handleOverlay);
-    this._CloseButton.addEventListener("click", this.close);
-  }
-
-  removeEventListeners() {
-    document.removeEventListener("keydown", this._handleEscClose);
-    this._popup.removeEventListener("mousedown", this._handleOverlay);
-    this._CloseButton.removeEventListener("click", this.close);
+    this._popupElement.addEventListener("click", (e) => {
+      if (e.target.classList.contains("modal__close")) {
+        this.close();
+      }
+    });
   }
 }
 
